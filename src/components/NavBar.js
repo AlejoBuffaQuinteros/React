c8170097a6bce761612a31a98a7067de08d7eb3d
AlejoBuffaQuinteros <buffaalejo0@gmail.com>
2023-12-06 23:01:41 -0300

    PreEntrega1

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+export const NavBar = () =>{
+   return <div>
+      <ul>
+         <li><a href="./index.js">Inicio</a></li>
+         <li><a href="./index.js">Catalogo</a></li>
+         <li><a href="./index.js">Contacto</a></li>
+         <li><a href="./index.js">Preguntas frecuentes</a></li>
+         <form> 
+            <input></input>
+            <button class="btn btn-outline-success" type="submit" value="Mostrar">Buscar</button>
+         </form>
+      </ul>
+      
+   </div>
+}
